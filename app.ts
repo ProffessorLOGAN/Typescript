edbf5728bc8bc60a869b793e03be9816dee960b0
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2024-03-26 20:18:54 +0530

    Type alias in Typescript

---
+++ app.ts
@@ -0,0 +1,23 @@
+// Type alias
+
+// type User = {
+//   name: string;
+//   age: number;
+//   address?: string;
+// };
+
+// const user: User = {
+//   name: "Tansen Keshri",
+//   age: 24,
+// };
+
+// function login(userData: User): User {
+//   return userData;
+// }
+
+// console.log(login(user));
+
+// type ID = number | string ;
+
+// const userId: ID = '123';
+
