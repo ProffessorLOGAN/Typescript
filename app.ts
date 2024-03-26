4514908d50e1c832f44f85d1cebd70fdbc7ffcad
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2024-03-26 20:34:03 +0530

    Interfaces in Typescript

--- app.ts
+++ app.ts
@@ -21,3 +21,30 @@
 
 // const userId: ID = '123';
 
+// Interfaces
+
+// interface Transaction {
+//   payerAccountNumber: number;
+//   payeeAccountNumber: number;
+// }
+
+// interface BankAccount {
+//   accountNumber: number;
+//   accountHolder: string;
+//   balance: number;
+//   isActive: boolean;
+//   transaction: Transaction[];
+// }
+
+// const transaction = {
+//   payerAccountNumber: 123,
+//   payeeAccountNumber: 500,
+// };
+
+// const bankAccount: BankAccount = {
+//   accountNumber: 123,
+//   accountHolder: "John Doe",
+//   balance: 1000,
+//   isActive: true,
+//   transaction: [transaction],
+// };
